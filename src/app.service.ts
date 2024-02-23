e8b5a39b357653bee9f76fa0214203b98e32aa9b
lemon <2294262671@qq.com>
2024-02-23 15:12:22 +0800

    第一次提交

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getTest(): string {
+    return 'Hello World26!';
+  }
+
+}
